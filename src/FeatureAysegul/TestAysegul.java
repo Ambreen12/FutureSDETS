b9d151ca3dffee1869bd50ce36876de6f89eb627
Aysegul Soytas Celik <aysegulsoytaszb@gmail.com>
2022-12-22 19:57:22 +0000

    Commit 2

--- src/FeatureAysegul/TestAysegul.java
+++ src/FeatureAysegul/TestAysegul.java
@@ -3,5 +3,6 @@ package FeatureAysegul;
 public class TestAysegul {
     public static void main(String[] args) {
         System.out.println("Me again");
+        System.out.println("What a wonderful life");
     }
 }
